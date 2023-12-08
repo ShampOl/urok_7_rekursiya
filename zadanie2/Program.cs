d80c7049d25c4391339e21f94fa44d2eaf4ef288
Olya <lolcap202@gmail.com>
2023-12-08 16:16:08 +0600

    add zadanie2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение m: ");
+        if (int.TryParse(Console.ReadLine(), out int m) && m >= 0)
+        {
+            Console.Write("Введите значение n: ");
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
+            {
+                int result = AckermannFunction(m, n);
+                Console.WriteLine($"Значение функции Аккермана для m={m} и n={n} равно {result}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод для n. Пожалуйста, введите неотрицательное целое число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод для m. Пожалуйста, введите неотрицательное целое число.");
+        }
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
