1055d8663f4b79e7112d7e7fbae4755d6c075490
Olya <lolcap202@gmail.com>
2023-12-08 16:21:14 +0600

    add zadanie3

---
+++ zadanie3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
